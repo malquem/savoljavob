bb6268e0f28771958a3b1718e40552d9b4916fc4
m0d <um-id@ya.ru>
2018-03-16 09:57:08 +0300

    page 65

--- app/src/main/java/ru/exp/savoljavob/QuizActivity.java
+++ app/src/main/java/ru/exp/savoljavob/QuizActivity.java
@@ -4,18 +4,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    // Добавление полей?
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    // Добавление перемменных и массива Question(QuizActivitz.java)
+    private Question[] mQuestionsBank = new Question[] {
+            //Программа несколько раз вызывает конструктор Question и создает массив
+            // объектов Question.
+            new Question(R.string.question_s1, true),
+            new Question(R.string.question_s2, true),
+            new Question(R.string.question_s2, false),
+            new Question(R.string.question_s2, false),
+            new Question(R.string.question_s2, true),
+            new Question(R.string.question_s2, true),
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        /* Подключение виджета TextView */
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionsBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+
+        // Получение ссылок на виджеты 1
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +50,8 @@ public class QuizActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
             }
         });
+
+        // Получение ссылок на виджеты 2
         mFalseButton = (Button)findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
